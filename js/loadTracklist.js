21e989d543bc6e52e1e54e349aa13dbc7e11d2bf
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-09-08 14:47:16 -0300

    Minor organizations

--- js/loadTracklist.js
+++ js/loadTracklist.js
@@ -1,33 +1,45 @@
+// Visibility management
 let tracklist_is_on = 0
 
-function hideTracklist(album_name, artist) {
-    const tracklist_container = document.getElementById('tracklist-container')
-    const tracklist_div = document.getElementById('tracklist')
+// Gets and displays the album tracklist
 
-    if (tracklist_is_on) {
-        tracklist_container.style.display = 'none'
-        tracklist_is_on = 0
-        tracklist_div.innerHTML = ''
-    } else {
-        tracklist_container.style.display = 'flex'
-        tracklist_is_on = 1
-        getTracklistHTML(album_name, artist)
+    function tracklist(album_name, artist) {
+
+        // HTML elements
+        const tracklist_container = document.getElementById('tracklist-container')
+        const tracklist_div = document.getElementById('tracklist')
+
+        // Visibility management
+        if (tracklist_is_on) {
+            tracklist_container.style.display = 'none'
+            tracklist_is_on = 0
+            tracklist_div.innerHTML = ''
+        } else {
+            tracklist_container.style.display = 'flex'
+            tracklist_is_on = 1
+            getTracklistHTML(album_name, artist)
+        }
+
     }
-}
 
-function getTracklistHTML(album_name, artist) {
+// 
 
-    const tracklist_div = document.getElementById('tracklist')
-    
-    const albumIndex = data.findIndex(album => album.name == album_name && album.artist == artist)
+// Loads the actual album tracklist
 
-    const tracklist = data[albumIndex].tracklist
-    let i = 0
+    function getTracklistHTML(album_name, artist) {
 
-    if (tracklist[0].track_rating == -1)
-    unrated = `&Oslash`
+        const tracklist_div = document.getElementById('tracklist')
+        
+        const albumIndex = data.findIndex(album => album.name == album_name && album.artist == artist)
 
-    tracklist.forEach(track => {
-        tracklist_div.innerHTML += `<span>${++i}. ${track.title} ${(track.track_rating == -1)? '' : '| ' + track.track_rating}</span>`
-    })
-}
+        const tracklist = data[albumIndex].tracklist
+        let i = 0
+
+        if (tracklist[0].track_rating == -1) unrated = `&Oslash`
+
+        tracklist.forEach(track => {
+            tracklist_div.innerHTML += `<span>${++i}. ${track.title} ${(track.track_rating == -1)? '' : '| ' + track.track_rating}</span>`
+        })
+    }
+
+// 
