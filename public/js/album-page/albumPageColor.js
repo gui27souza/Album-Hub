a654b677a665568e7f0f64dcf84a10a61019ba78
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-09-17 16:02:31 -0300

    Theme/Color update

--- public/js/album-page/albumPageColor.js
+++ public/js/album-page/albumPageColor.js
@@ -5,7 +5,8 @@ async function getAlbumColors() {
     img.onload = () => {
 
         Vibrant.from(img)
-        .maxColorCount(3)
+        .maxColorCount(4)
+        .quality(50)
         .getPalette()
 
         .then((palette) => {
@@ -23,47 +24,19 @@ async function getAlbumColors() {
 
 function albumColors(palette) {
     
-    const vibrant = `rgb(${palette.Vibrant.rgb[0]}, ${palette.Vibrant.rgb[1]}, ${palette.Vibrant.rgb[2]})`
-    const vibrant_population = palette.Vibrant.population
+    let light_background = [palette.Vibrant, palette.Muted, palette.LightVibrant, palette.LightMuted]
+    light_background = light_background.reduce((maior, atual) => {
+        return (atual.population > maior.population) ? atual : maior
+    })
+    light_background = `rgb(${light_background.rgb[0]}, ${light_background.rgb[1]}, ${light_background.rgb[2]})`
 
-    const vibrant_dark = `rgb(${palette.DarkVibrant.rgb[0]}, ${palette.DarkVibrant.rgb[1]}, ${palette.DarkVibrant.rgb[2]})`
-    const vibrant_dark_population = palette.DarkVibrant.population
+    let dark_background = [palette.DarkVibrant, palette.DarkMuted]
+    dark_background = dark_background.reduce((maior, atual) => {
+        return (atual.population > maior.population) ? atual : maior
+    })
+    dark_background = `rgb(${dark_background.rgb[0]}, ${dark_background.rgb[1]}, ${dark_background.rgb[2]})`
 
-    const vibrant_light = `rgb(${palette.LightVibrant.rgb[0]}, ${palette.LightVibrant.rgb[1]}, ${palette.LightVibrant.rgb[2]})`
-    const vibrant_light_population = palette.LightVibrant.population
 
-    const muted = `rgb(${palette.Muted.rgb[0]}, ${palette.Muted.rgb[1]}, ${palette.Muted.rgb[2]})`
-    const muted_population = palette.Muted.population
-
-    const muted_dark = `rgb(${palette.DarkMuted.rgb[0]}, ${palette.DarkMuted.rgb[1]}, ${palette.DarkMuted.rgb[2]})`
-    const muted_dark_population = palette.DarkMuted.population
-
-    const muted_light = `rgb(${palette.LightMuted.rgb[0]}, ${palette.LightMuted.rgb[1]}, ${palette.LightMuted.rgb[2]})`
-    const muted_light_population = palette.LightMuted.population
-    
-    document.documentElement.style.setProperty('--vibrant', vibrant)
-    document.documentElement.style.setProperty('--dark-vibrant', vibrant_dark)
-    document.documentElement.style.setProperty('--light-vibrant', vibrant_light)
-    
-    document.documentElement.style.setProperty('--muted', muted)
-    document.documentElement.style.setProperty('--dark-muted', muted_dark)
-    document.documentElement.style.setProperty('--light-muted', muted_light)
-
-    if (muted_light_population > vibrant_light_population) document.documentElement.style.setProperty('--album-sub-items', muted_light)
-    else document.documentElement.style.setProperty('--album-sub-items', vibrant_light)
-    
-    
-    document.documentElement.style.setProperty('--album-background-dark', 1)
-    document.documentElement.style.setProperty('--album-background-light', 1)
-    document.documentElement.style.setProperty('--album-container', 1)
-    document.documentElement.style.setProperty('--album-sub-items', 1)
-}
-
-function paletteTester(palette) {
-
-    for (const color in palette) {
-        const HTML_element = document.getElementById(`${color}`);
-        HTML_element.innerHTML = `${palette[color].population}`;
-    }
-
+    document.documentElement.style.setProperty('--album-background-light', light_background)
+    document.documentElement.style.setProperty('--album-background-dark', dark_background)
 }
