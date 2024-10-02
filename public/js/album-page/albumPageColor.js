1055258b83b0186434b56c83af6478a041174542
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-10-02 14:18:29 -0300

    Update albumPageColor.js

--- public/js/album-page/albumPageColor.js
+++ public/js/album-page/albumPageColor.js
@@ -18,7 +18,7 @@ async function getAlbumColors() {
         })
 
         .catch((err) => {
-            console.error('Erro ao extrair a paleta:', err)
+            console.error('ERROR palette:', err)
         })
         
     }
@@ -30,15 +30,15 @@ function albumColors(palette) {
     
     // Checks the best option for light color
     let light_background = [palette.Vibrant, palette.Muted, palette.LightVibrant, palette.LightMuted]
-    light_background = light_background.reduce((maior, atual) => {
-        return (atual.population > maior.population) ? atual : maior
+    light_background = light_background.reduce((max, current) => {
+        return (current.population > max.population) ? current : max
     })
     light_background = `rgb(${light_background.rgb[0]}, ${light_background.rgb[1]}, ${light_background.rgb[2]})`
 
     // Checks the best option for dark color
     let dark_background = [palette.DarkVibrant, palette.DarkMuted]
-    dark_background = dark_background.reduce((maior, atual) => {
-        return (atual.population > maior.population) ? atual : maior
+    dark_background = dark_background.reduce((max, current) => {
+        return (current.population > max.population) ? current : max
     })
     dark_background = `rgb(${dark_background.rgb[0]}, ${dark_background.rgb[1]}, ${dark_background.rgb[2]})`
 
