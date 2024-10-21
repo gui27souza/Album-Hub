96242c1006219980b2f6025980ce98463a360adb
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-10-21 09:39:32 -0300

    Rate album interface start

---
+++ public/js/album-page/rate-album.js
@@ -0,0 +1,12 @@
+const rate_album_show = document.getElementById('rate-album-show')
+const rate_album_hide = document.getElementById('rate-album-hide')
+
+const rate_album_container = document.getElementById('rate-album-container')
+
+function showRateAlbum() {
+    rate_album_container.style.display = 'flex'
+}
+
+function hideRateAlbum() {
+    rate_album_container.style.display = 'none'
+}
