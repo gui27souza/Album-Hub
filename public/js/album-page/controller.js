cc96f2de12ab450dca0a42fbb8448756e956f2c2
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-10-31 14:34:36 -0300

    Rate album - front to server communication

--- public/js/album-page/controller.js
+++ public/js/album-page/controller.js
@@ -17,3 +17,39 @@
     }
 
 // 
+
+    async function putAlbumRate(album_rate, album_name, artist) {
+
+        try {
+            const response = await fetch(`/data/updateAlbum/album/albumRate/?album_name=${encodeURIComponent(album_name)}&artist=${encodeURIComponent(artist)}`, {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({rate: album_rate})
+            })
+
+            return
+        } 
+        
+        catch(error) {
+            console.error('ERROR: error updating album rate', error)
+            return
+        }
+    }
+
+    async function putTracklistRate(tracklist, album_name, artist) {
+        
+        try {
+            const response = await fetch(`/data/updateAlbum/album/tracklistRate/?album_name=${encodeURIComponent(album_name)}&artist=${encodeURIComponent(artist)}`, {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({tracklist: tracklist})
+            })
+
+            return
+        } 
+        
+        catch(error) {
+            console.error('ERROR: error updating album tracklist rate', error)
+            return
+        }
+    }
