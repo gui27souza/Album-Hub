5e50b291476932b47513f80bd74d9594e9f93174
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-10-31 14:57:24 -0300

    Modify data - documentation

--- source/modify-data.js
+++ source/modify-data.js
@@ -10,6 +10,7 @@ const { readData, readSettings, updateData, updateSettings } = require("./file-h
 
         const data = readData()
         
+        // Organize data
         const album_name = album_data.name
         const artist = album_data.artist        
         const cover = album_data.image[4]['#text']
@@ -17,6 +18,7 @@ const { readData, readSettings, updateData, updateSettings } = require("./file-h
         const tags = formatTags(album_data.tags.tag)
         const wiki = album_data.wiki? album_data.wiki.content : -1
         
+        // Create album object with data
         const newAlbum = { 
             name: album_name,
             artist: artist,
@@ -29,9 +31,9 @@ const { readData, readSettings, updateData, updateSettings } = require("./file-h
             wiki: wiki
         }
 
+        // Album added successfully
         data.albums.push(newAlbum)
         updateData(data)
-
         return true
     }
 
@@ -40,37 +42,27 @@ const { readData, readSettings, updateData, updateSettings } = require("./file-h
 // Format tracklist and tags sent from front-end to data format
 
     function formatTracklist(tracklist) {
-
         let formated_tracklist = []
-
         let i = 1
-        tracklist.forEach(track => {
-            
+        tracklist.forEach(track => {            
             let formated_track = {
                 number: i,
                 title: track.name,
                 track_rate: -1
             }
-
             formated_tracklist.push(formated_track)
             i++
         })
-
         return formated_tracklist
     }
 
     function formatTags(tags) {
-
         if (tags == undefined) return []
-
         const tags_lenght = tags.length
-
         let formated_tags = []
-
         for (let i = 0; i < tags_lenght; i++) {
             formated_tags.push(tags[i].name)
         }
-    
         return formated_tags
     }
 
@@ -82,10 +74,14 @@ const { readData, readSettings, updateData, updateSettings } = require("./file-h
 
         const data = readData()
         
-        album_index = data.albums.findIndex(album => album.name === album_name && album.artist === artist)
+        const album_index = data.albums.findIndex(album => album.name === album_name && album.artist === artist)
+        
+        // Album is not in the library
+        if (album_index == -1) return false
+        
+        // Album deleted successfully
         data.albums.splice(album_index, 1)
         updateData(data)
-
         return true
     }
     
@@ -97,14 +93,15 @@ const { readData, readSettings, updateData, updateSettings } = require("./file-h
         
         const data = readData()
 
-        album_index = data.albums.findIndex(album => album.name === album_name && album.artist === artist)
+        const album_index = data.albums.findIndex(album => album.name === album_name && album.artist === artist)
 
+        // Error - album not in the library
         if (album_index == -1) return false
 
         data.albums[album_index].rate = Number(rate)
 
+        // Album rate updated successfully
         updateData(data)
-
         return true
     }
 
@@ -116,23 +113,25 @@ const { readData, readSettings, updateData, updateSettings } = require("./file-h
 
         const data = readData()
 
-        album_index = data.albums.findIndex(album => album.name === album_name && album.artist === artist)
+        const album_index = data.albums.findIndex(album => album.name === album_name && album.artist === artist)
 
+        // Error - album not in the library
         if (album_index == -1) return false
 
+        // Update tracklist
         data.albums[album_index].tracklist = tracklist
 
+        // Calculate the average track rate
         let rate_sum = 0
         let track_counting = 0
         data.albums[album_index].tracklist.forEach(track => {
             rate_sum += track.track_rate
             track_counting++
         })
-
         data.albums[album_index].average_track_rate = Number((rate_sum/track_counting).toFixed(1))
 
+        // Tracklist rate updated successfully
         updateData(data)
-
         return true
     }
 
